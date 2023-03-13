590afc7fa9424014eb7b78a04b7ef12b32cdf503
Samuel Van Campenhout <vancampenhoutsamuel@gmail.com>
2023-03-13 11:24:57 +0100

    add - actors list

--- lib/pages/movie/movie_page.dart
+++ lib/pages/movie/movie_page.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:projet_lepl1509_groupe_17/components/drawer/drawer.dart';
 import 'package:projet_lepl1509_groupe_17/models/movies.dart';
-import 'package:projet_lepl1509_groupe_17/models/review.dart';
-import 'package:projet_lepl1509_groupe_17/pages/home/create_review_page.dart';
-import 'package:projet_lepl1509_groupe_17/pages/home/home_page.dart';
-import 'package:projet_lepl1509_groupe_17/pages/movie/movie_page.dart';
 import 'package:projet_lepl1509_groupe_17/pages/search/search_page.dart';
 
 User? currentUser = FirebaseAuth.instance.currentUser;
@@ -22,81 +15,99 @@ class MoviePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF000B49),
-        actions: [
-          IconButton(
-              color: Colors.white,
-              onPressed: () {
-                Get.to(() => const SearchPage());
-              },
-              icon: const Icon(Icons.arrow_back))
-        ],
-      ),
       body: Stack(
         children: [
           ..._buildBackground(context, movie),
+          _buildMovieInformation(context),
           Positioned(
-            bottom: 150,
+            bottom: 0,
             width: MediaQuery.of(context).size.width,
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
                 children: [
                   Text(
-                    movie.title,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    "${movie.release_date.toDate().day}/${movie.release_date.toDate().month}/${movie.release_date.toDate().year} | ${movie.category} | ${movie.duration}",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                  const SizedBox(height: 10.0),
-                  RatingBar.builder(
-                    initialRating: 3.5,
-                    minRating: 1,
-                    maxRating: 5,
-                    direction: Axis.horizontal,
-                    allowHalfRating: true,
-                    ignoreGestures: true,
-                    itemCount: 5,
-                    itemSize: 20,
-                    unratedColor: Colors.white,
-                    itemPadding: const EdgeInsets.symmetric(
-                      horizontal: 4.0,
-                    ),
-                    itemBuilder: (context, index) {
-                      return const Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      );
-                    },
-                    onRatingUpdate: (rating) {},
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    movie.summary,
-                    maxLines: 10,
+                    'Actors',
                     style: Theme.of(context)
                         .textTheme
-                        .bodySmall!
-                        .copyWith(height: 1.75, color: Colors.white),
+                        .labelMedium
+                        ?.copyWith(fontSize: 18.0, color: Colors.white),
+                  ),
+                  SizedBox.fromSize(
+                    size: const Size.fromHeight(120),
+                    child: ListView.builder(
+                      itemCount: movie.actors.length,
+                      scrollDirection: Axis.horizontal,
+                      padding: const EdgeInsets.only(top: 12.0, left: 20.0),
+                      itemBuilder: _buildActor,
+                    ),
                   ),
                 ],
               ),
             ),
-          )
+          ),
         ],
+      ),
+    );
+  }
+
+  Positioned _buildMovieInformation(BuildContext context) {
+    return Positioned(
+      bottom: 150,
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text(
+              movie.title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "${movie.release_date.toDate().day}/${movie.release_date.toDate().month}/${movie.release_date.toDate().year} | ${movie.category} | ${movie.duration}",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            RatingBar.builder(
+              initialRating: 3.5,
+              minRating: 1,
+              maxRating: 5,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              ignoreGestures: true,
+              itemCount: 5,
+              itemSize: 20,
+              unratedColor: Colors.white,
+              itemPadding: const EdgeInsets.symmetric(
+                horizontal: 4.0,
+              ),
+              itemBuilder: (context, index) {
+                return const Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                );
+              },
+              onRatingUpdate: (rating) {},
+            ),
+            const SizedBox(height: 20),
+            Text(
+              movie.summary,
+              maxLines: 8,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(height: 1.75, color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -127,5 +138,29 @@ class MoviePage extends StatelessWidget {
         )),
       )),
     ];
+  }
+
+  Widget _buildActor(BuildContext context, int index) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: Column(
+        children: [
+          const CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/n8YlGookYzgD3cmpMP45BYRNIoh.jpg'),
+            radius: 40,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              movie.actors[index].toString(),
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
